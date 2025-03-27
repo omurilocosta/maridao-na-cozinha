d3365dfdae81b17443ec4e89ff07dce3910a7035
Murilo Costa <murilocosta2707@gmail.com>
2025-03-26 23:03:54 -0300

    Add files via upload

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,14 +1,20 @@
 function adicionarIngrediente() {
     let div = document.createElement("div")
     div.innerHTML = `
-        <input type="text" name="ingredientes[]" placeholder="Ingrediente">
-        <input type="text" name="quantidades[]" placeholder="Quantidade">`
-    document.getElementById("ingredientes").appendChild(div)
+        <div class="inputs-ingredientes">
+            <input type="text" name="ingredientes[]" placeholder="Ingrediente">
+            <input type="text" name="quantidades[]" placeholder="Quantidade">
+        </div>
+                `
+    document.getElementById("add-ingredientes-area").appendChild(div)
 }
 
 function adicionarPasso() {
-    let i = 1
+    let i = document.querySelectorAll(".inputs-preparos").length + 1
     let div = document.createElement("div")
-    div.innerHTML = `<input type="text" name="passos[]" placeholder="Passo ${i}">`
-    document.getElementById("passos").appendChild(div)
+    div.innerHTML = `
+                <div class="inputs-preparos">
+                    <input type="text" name="passos[]" placeholder="Passo ${i}">
+                </div>`
+    document.getElementById("add-preparos-area").appendChild(div)
 }
